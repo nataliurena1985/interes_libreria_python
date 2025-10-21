8ebf3b7ef912a4ab5f1a8cedd469da9cbbe7953e
nataliurena1985 <claudianataliurena@gmail.com>
2025-10-21 20:06:19 -0300

    cua commit

--- app1.py
+++ app1.py
@@ -9,5 +9,5 @@ tasa=st.number_input("Ingrese la tasa")
 tiempo=st.number_input("Ingrese el tiempo")
 
 
-resultado = DMC.interes_simple(monto,tasa,tiempo)
+resultado = DMC.calcular_interes(monto,tasa,tiempo)
 st.write(resultado)
